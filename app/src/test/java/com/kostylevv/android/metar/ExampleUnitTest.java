7ecd68c2da9fe211596d7d740c237a361068f889
Vladislav Kostylev <kostylev.v@gmail.com>
2017-01-17 15:15:36 +0300

    Added parser from https://github.com/arimus/jweather

--- app/src/test/java/com/kostylevv/android/metar/ExampleUnitTest.java
+++ app/src/test/java/com/kostylevv/android/metar/ExampleUnitTest.java
@@ -1,8 +1,10 @@
 package com.kostylevv.android.metar;
 
 import org.junit.Test;
+import parser.Metar;
+import parser.MetarParser;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +12,23 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void metar_station_id_isCorrect() throws Exception {
+        Metar metar = new Metar();
+        metar = MetarParser.parseReport("EFHK 171150Z 20006KT 9999 BKN007 M05/M06 Q1030 NOSIG 17-01-17");
+        assertEquals(metar.getStationID(), "EFHK");
+    }
+
+    @Test
+    public void metar_date_isCorrect() throws Exception {
+        Metar metar = new Metar();
+        metar = MetarParser.parseReport("ULLI 171130Z 19002MPS 160V230 9999 OVC022 M04/M06 Q1031 R88/550348 NOSIG 17-01-17");
+        assertEquals((long)metar.getWindDirection(), (long) 190);
     }
 }
