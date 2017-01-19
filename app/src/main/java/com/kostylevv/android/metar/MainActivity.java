5d7e82236c8ae8e9a4b1a04360c0853513f3f167
Vladislav Kostylev <kostylev.v@gmail.com>
2017-01-19 16:12:08 +0300

    Edited TODO's

--- app/src/main/java/com/kostylevv/android/metar/MainActivity.java
+++ app/src/main/java/com/kostylevv/android/metar/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     /**
-     * 19/01 TODO add Fragment with RecyclerView
+     * 19/01 TODO add RecyclerView
      * 19/01 TODO add Loader for APTS list with API URL https://v4p4sz5ijk.execute-api.us-east-1.amazonaws.com/anbdata/airports/weather/metar-stations-list?api_key=aaec15f0-d987-11e6-b3ce-4fe0c5a3f2bb&airports=&states=&format=json
      * 20/01 TODO add Search
      * 20/01 TODO add DetailFragment
